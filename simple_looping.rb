d7d6bdf1e9b6529607ea29b0381e0815e6b78873
David Sim <wsim903@gmail.com>
2018-09-25 19:40:23 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for c in 1..number_of_times
+  for c in range number_of_times
     puts phrase
   end
 end
